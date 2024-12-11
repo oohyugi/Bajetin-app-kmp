fe14b48d2574f33f44a7d8e34d6549e8677b13db
oohyugi <mail.yogiputra@gmail.com>
2024-12-11 12:54:38 +0700

    test-cicd: update path

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ val detektTask = tasks.register<JavaExec>("detekt") {
     val config = "config/detekt/detekt.yml"
     val exclude = "**/build/**,**/resources/**"
     val report =
-        "xml:build/reports/detekt.xml,html:build/reports/detekt.html,txt:build/reports/detekt.txt,sarif:build/reports/detekt.sarif"
+        "xml:build/reports/detekt.xml,html:build/reports/detekt.html,txt:build/reports/detekt.txt,sarif:build/reports/detekt/detekt.sarif"
 
     val autoFormatEnabled = project.hasProperty("auto-correct")
 
