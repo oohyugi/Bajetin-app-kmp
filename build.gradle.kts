98703b427a06b8b6996d64579018ea8cd02df95d
Yogi Putra <mail.yogiputra@gmail.com>
2024-12-12 16:49:22 +0700

    Feat: Add Kover (#3)
    * add kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.kover) apply false
 }
 
 val detekt by configurations.creating
